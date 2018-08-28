ebae61d399356b1a73419b569bf7bc0d5051b103
Giuseppe Filograno <filograno@gmail.com>
2018-08-28 12:39:24 +0200

    Add recycler view for the talk list

--- app/src/main/java/com/github/gdgitalia/gdgeventtracker/TalkListFragment.kt
+++ app/src/main/java/com/github/gdgitalia/gdgeventtracker/TalkListFragment.kt
@@ -6,6 +6,8 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.firebase.ui.firestore.FirestoreRecyclerOptions
+import com.github.gdgitalia.gdgeventtracker.model.Talk
+import com.google.firebase.firestore.FirebaseFirestore
 
 /**
  * A simple [Fragment] subclass.
@@ -18,14 +20,20 @@ import com.firebase.ui.firestore.FirestoreRecyclerOptions
  */
 class TalkListFragment : Fragment() {
 
+
+
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
         // Inflate the layout for this fragment
-        FirestoreRecyclerOptions.Builder<String>()
+        val query = FirebaseFirestore.getInstance().collection(TALKS_KEY).orderBy(DATE_FIELD)
+        FirestoreRecyclerOptions.Builder<Talk>().setQuery(query, Talk::class.java).build()
         return inflater.inflate(R.layout.fragment_talk_list, container, false)
     }
 
     companion object {
+
+        const val TALKS_KEY = "talks"
+        const val DATE_FIELD = "date"
         /**
          * Use this factory method to create a new instance of
          * this fragment using the provided parameters.
---
+++ app/src/main/java/com/github/gdgitalia/gdgeventtracker/adapter/TalkRecyclerAdapter.kt
@@ -0,0 +1,25 @@
+package com.github.gdgitalia.gdgeventtracker.adapter
+
+import android.support.v7.widget.RecyclerView
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.firebase.ui.firestore.FirestoreRecyclerAdapter
+import com.firebase.ui.firestore.FirestoreRecyclerOptions
+import com.github.gdgitalia.gdgeventtracker.R
+import com.github.gdgitalia.gdgeventtracker.model.Talk
+import kotlinx.android.synthetic.main.item_talk.view.*
+
+class TalkRecyclerAdapter(options: FirestoreRecyclerOptions<Talk>) : FirestoreRecyclerAdapter<Talk, TalkHolder>(options) {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): TalkHolder {
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.item_talk, parent, false)
+        return TalkHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: TalkHolder, position: Int, model: Talk) {
+    }
+}
+
+class TalkHolder(itemView: View?) : RecyclerView.ViewHolder(itemView) {
+
+}
