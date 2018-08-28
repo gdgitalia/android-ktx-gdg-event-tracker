c2e2adac1e318a2ae669e6051bff76f467fe473e
omarmiatello <jackl85@gmail.com>
2018-08-28 12:08:22 +0200

    test

--- app/src/main/java/com/github/gdgitalia/gdgeventtracker/TalkListFragment.kt
+++ app/src/main/java/com/github/gdgitalia/gdgeventtracker/TalkListFragment.kt
@@ -1,12 +1,11 @@
 package com.github.gdgitalia.gdgeventtracker
 
-import android.content.Context
-import android.net.Uri
 import android.os.Bundle
 import android.support.v4.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.firebase.ui.firestore.FirestoreRecyclerOptions
 
 /**
  * A simple [Fragment] subclass.
@@ -22,6 +21,7 @@ class TalkListFragment : Fragment() {
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
         // Inflate the layout for this fragment
+        FirestoreRecyclerOptions.Builder<String>()
         return inflater.inflate(R.layout.fragment_talk_list, container, false)
     }
 
