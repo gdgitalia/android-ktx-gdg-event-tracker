4b4d9bfb2227a703dd1c9febfbd84fcf1d46d227
omarmiatello <jackl85@gmail.com>
2018-08-29 22:54:06 +0200

    Clean import

--- app/src/main/java/com/github/gdgitalia/gdgeventtracker/adapter/TalkRecyclerAdapter.kt
+++ app/src/main/java/com/github/gdgitalia/gdgeventtracker/adapter/TalkRecyclerAdapter.kt
@@ -4,6 +4,7 @@ import android.support.v7.widget.RecyclerView
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.bumptech.glide.Glide
 import com.firebase.ui.firestore.FirestoreRecyclerAdapter
 import com.firebase.ui.firestore.FirestoreRecyclerOptions
 import com.github.gdgitalia.gdgeventtracker.R
@@ -14,7 +15,10 @@ import java.text.SimpleDateFormat
 import java.util.*
 
 
-class TalkRecyclerAdapter(options: FirestoreRecyclerOptions<Talk>, private val navigator: TalkItemNavigator) : FirestoreRecyclerAdapter<Talk, TalkHolder>(options) {
+class TalkRecyclerAdapter(
+    options: FirestoreRecyclerOptions<Talk>,
+    private val navigator: TalkItemNavigator
+) : FirestoreRecyclerAdapter<Talk, TalkHolder>(options) {
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): TalkHolder {
         val view = LayoutInflater.from(parent.context).inflate(R.layout.item_talk, parent, false)
@@ -25,7 +29,7 @@ class TalkRecyclerAdapter(options: FirestoreRecyclerOptions<Talk>, private val n
         holder.title.text = model.title
         holder.speaker.text = model.speakerName
         holder.level.rating = model.level.toFloat()
-        val sdf= SimpleDateFormat("EEE d MMM - HH:mm", Locale.getDefault())
+        val sdf = SimpleDateFormat("EEE d MMM - HH:mm", Locale.getDefault())
         holder.date.text = sdf.format(model.date)
 
         holder.itemView.setOnClickListener {
@@ -35,7 +39,8 @@ class TalkRecyclerAdapter(options: FirestoreRecyclerOptions<Talk>, private val n
 
         holder.location.text = model.location
         holder.level.setIsIndicator(false)
-        val countryPath = "https://raw.githubusercontent.com/madebybowtie/FlagKit/master/Assets/PNG/${model.country}.png"
+        val countryPath =
+            "https://raw.githubusercontent.com/madebybowtie/FlagKit/master/Assets/PNG/${model.country}.png"
         Glide.with(holder.itemView.context).load(countryPath).into(holder.country)
         Glide.with(holder.itemView.context).load(model.pic).into(holder.pic)
     }
