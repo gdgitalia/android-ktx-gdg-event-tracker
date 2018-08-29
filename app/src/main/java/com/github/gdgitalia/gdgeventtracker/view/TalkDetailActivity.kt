4d9aad523fc30fff61cd9860b1192540e518c983
omarmiatello <jackl85@gmail.com>
2018-08-29 13:40:41 +0200

    new detail

--- app/src/main/java/com/github/gdgitalia/gdgeventtracker/view/TalkDetailActivity.kt
+++ app/src/main/java/com/github/gdgitalia/gdgeventtracker/view/TalkDetailActivity.kt
@@ -2,9 +2,11 @@ package com.github.gdgitalia.gdgeventtracker.view
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import com.bumptech.glide.Glide
 import com.github.gdgitalia.gdgeventtracker.R
 import com.github.gdgitalia.gdgeventtracker.model.Talk
 import com.google.firebase.firestore.FirebaseFirestore
+import kotlinx.android.synthetic.main.activity_talk_detail.*
 
 class TalkDetailActivity : AppCompatActivity() {
 
@@ -25,8 +27,25 @@ class TalkDetailActivity : AppCompatActivity() {
 
     private fun populateActivity(talk: Talk?) {
         talk?.apply {
-//            titleTextView.text = title
-//            speakerTextView.text = speaker
+            talkTitleTextView.text = title
+            talkAbstractTextView.text = abstract
+            talkTechnologyTextView.text = technology
+            talkTypeTextView.text = type
+            talkDateTextView.text = date.toString()
+            talkHashtagTextView.text = hashtag
+            talkLanguageTextView.text = language
+            val countryPath = "https://raw.githubusercontent.com/madebybowtie/FlagKit/master/Assets/PNG/$country.png"
+            talkCountryTextView.text = country
+            talkLevelTextView.text = level.toString()
+            Glide.with(this@TalkDetailActivity).load(pic).into(talkPicImageView)
+            talkLocationTextView.text = location
+
+            speakerNameTextView.text = speakerName
+            speakerBioTextView.text = speakerBio
+            speakerCommunityTextView.text = speakerCommunity
+            speakerCompanyTextView.text = speakerCompany
+            speakerTypeTextView.text = speakerType
+            Glide.with(this@TalkDetailActivity).load(speakerPic).into(speakerPicImageView)
         }
 
     }
