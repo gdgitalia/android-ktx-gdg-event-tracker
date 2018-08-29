5c308923e4dba3ca15fa22f723978c031a0f1a5f
omarmiatello <jackl85@gmail.com>
2018-08-29 11:53:52 +0200

    Add country in Talk

--- app/src/main/java/com/github/gdgitalia/gdgeventtracker/model/Talk.kt
+++ app/src/main/java/com/github/gdgitalia/gdgeventtracker/model/Talk.kt
@@ -10,6 +10,7 @@ data class Talk(
         val date: Date = Date(),
         val hashtag: String = "",
         val language: String = "",
+        val country: String = "",
         val level: Long = 0,
         val speaker: String = ""
 )
